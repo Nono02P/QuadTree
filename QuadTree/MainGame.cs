5708c9e6e9f8aa3dce99099f6569c1e417f833f3
Nono02P <nono02p@hotmail.fr>
2019-07-03 22:24:40 +0200

    Première version.

---
+++ QuadTree/MainGame.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
+namespace QuadTreeSearch
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class MainGame : Game
+    {
+        private const bool STATIC_TREE = true;
+        private const int NUMBER_OF_POINTS = 10000;
+
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Random rnd;
+
+        private Viewport _viewport;
+        private QuadTree _quadTree;
+        private MouseState _oldMouse;
+        private Rectangle _cursor;
+        private List<Point> _points;
+
+        public MainGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+        
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            _viewport = GraphicsDevice.Viewport;
+            _cursor = new Rectangle(0, 0, 100, 100);
+            _points = new List<Point>();
+            rnd = new Random();
+            if (STATIC_TREE)
+                GenerateTree();
+        }
+
+        private void GenerateTree()
+        {
+            _quadTree = new QuadTree(_viewport.Bounds, 5);
+
+            for (int i = 0; i < NUMBER_OF_POINTS; i++)
+            {
+                Point p = new Point((int)(rnd.NextDouble() * _viewport.Width), (int)(rnd.NextDouble() * _viewport.Height));
+                _quadTree.Insert(ref p);
+            }
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            MouseState mouse = Mouse.GetState();
+            if (!STATIC_TREE)
+                GenerateTree();
+            else
+            {
+                if (mouse.LeftButton == ButtonState.Pressed && _oldMouse.LeftButton == ButtonState.Released)
+                {
+                    Point p = mouse.Position;
+                    _quadTree.Insert(ref p);
+                }
+            }
+
+            _cursor.Location = mouse.Position;
+
+            _points.Clear();
+            _quadTree.GetPoints(_cursor, _points);
+
+            _oldMouse = mouse;
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            Console.WriteLine(gameTime.IsRunningSlowly + " : " + gameTime.ElapsedGameTime.ToString());
+
+            spriteBatch.Begin();
+            _quadTree.Draw(spriteBatch);
+            spriteBatch.DrawRectangle(_cursor, Color.Green);
+
+            int total = _points.Count;
+            int collide = 0;
+            int notCollide = 0;
+
+            for (int i = 0; i < _points.Count; i++)
+            {
+                Point p = _points[i];
+                Color color = Color.Yellow;
+                if (_cursor.Contains(p))
+                {
+                    color = Color.Green;
+                    collide++;
+                }
+                else
+                {
+                    notCollide++;
+                }
+                spriteBatch.DrawCircle(p.ToVector2(), 2, 5, color);
+            }
+            //Console.WriteLine("Total : " + total + " Collide : " + collide + " Not Collide : " + notCollide);
+            spriteBatch.End();
+            
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ QuadTree/QuadTree.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+
+namespace QuadTreeSearch
+{
+    public class QuadTree
+    {
+        private bool _isSplitted;
+        private Rectangle _quadArea;
+        private QuadTree[] _quads;
+        private uint _quadCapacity;
+        private Point[] _points;
+        private int _pointIndex;
+
+        public QuadTree(Rectangle pQuadArea, uint pCapacity = 4)
+        {
+            _quadArea = pQuadArea;
+            _quadCapacity = pCapacity;
+            _quads = new QuadTree[4];
+            _points = new Point[_quadCapacity];
+        }
+
+        public void Insert(ref Point pPoint)
+        {
+            if (_quadArea.Contains(pPoint))
+            {
+                if (_pointIndex < _quadCapacity && !_isSplitted)
+                {
+                    _points[_pointIndex] = pPoint;
+                    _pointIndex++;
+                }
+                else
+                {
+                    if (!_isSplitted)
+                    {
+                        if (_quadArea.Width == 1 || _quadArea.Height == 1)
+                            throw new Exception("The capacity is too low, to much quads are created.");
+                        else
+                        {
+                            int x = _quadArea.X;
+                            int y = _quadArea.Y;
+                            int w = (int)Math.Ceiling((decimal)_quadArea.Width / 2);
+                            int h = (int)Math.Ceiling((decimal)_quadArea.Height / 2);
+
+                            for (int l = 0; l < 2; l++)
+                            {
+                                for (int c = 0; c < 2; c++)
+                                {
+                                    _quads[c + l * 2] = new QuadTree(new Rectangle(x + w * c, y + h * l, w, h), _quadCapacity);
+                                }
+                            }
+                            _isSplitted = true;
+
+                            for (int i = 0; i < _pointIndex; i++)
+                            {
+                                for (int j = 0; j < 4; j++)
+                                {
+                                    _quads[j].Insert(ref _points[i]);
+                                }
+                            }
+                            _points = null;
+                            _pointIndex = -1;
+                        }
+                    }
+                    for (int j = 0; j < 4; j++)
+                    {
+                        _quads[j].Insert(ref pPoint);
+                    }
+                }
+            }
+        }
+
+        public void GetPoints(Rectangle pArea, List<Point> pOutList)
+        {
+            if (pOutList == null)
+                pOutList = new List<Point>();
+
+            if (_quadArea.Intersects(pArea))
+            {
+                if (!_isSplitted)
+                {
+                    for (int i = 0; i < _pointIndex; i++)
+                    {
+                        pOutList.Add(_points[i]);
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < _quads.Length; i++)
+                    {
+                        _quads[i].GetPoints(pArea, pOutList);
+                    }
+                }
+            }
+        }
+
+        private void countSubQuads(ref int pCounter)
+        {
+            if (!_isSplitted)
+            {
+                pCounter += _pointIndex;
+            }
+            else
+            {
+                for (int i = 0; i < _quads.Length; i++)
+                {
+                    _quads[i].countSubQuads(ref pCounter);
+                }
+            }
+        }
+
+        public int Count()
+        {
+            int counter = 0;
+            countSubQuads(ref counter);
+            return counter;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.DrawRectangle(_quadArea, Color.White);
+            if (!_isSplitted)
+            {
+                for (int i = 0; i < _pointIndex; i++)
+                {
+                    spriteBatch.DrawCircle(_points[i].ToVector2(), 2, 5, Color.White);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < _quads.Length; i++)
+                {
+                    _quads[i].Draw(spriteBatch);
+                }
+            }
+        }
+    }
+}
